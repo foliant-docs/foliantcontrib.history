f11e6f8ec40ccb6bf09d784f3b86e7b8ac3b11e5
Artemy Lomov <lomov@restream.rt.ru>
2019-09-13 13:14:14 +0300

    Bump version to 1.0.3.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.0.2',
+    version='1.0.3',
     author='Artemy Lomov',
     author_email='artemy@lomov.ru',
     url='https://github.com/foliant-docs/foliantcontrib.history',
